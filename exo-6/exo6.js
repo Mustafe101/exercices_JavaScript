21d6caec0a603a1b9aa77ecac39ee2f9e9884742
mustafe <bonisher420@outlook.com>
2022-06-21 22:17:57 +0200

    finished

--- exo-6/exo6.js
+++ exo-6/exo6.js
@@ -18,6 +18,7 @@ _games: [{
     opponentPoints: 19,
 }]
 }
+
 function addPlayer(first, last, age){
     var player={};
     player.firstName = first;
@@ -28,33 +29,66 @@ function addPlayer(first, last, age){
 }
 
 
-
-var newPlayer = addPlayer ('yaye', 'bigy', 24);
-var newPlayerA = addPlayer ('daaha', 'tal', 19);
-var newPlayerB = addPlayer ('timo', 'dhex', 22);
+var newPlayer = new addPlayer ('yaye', 'bigy', 24);
+var newPlayerA = new addPlayer ('daaha', 'tal', 19);
+var newPlayerB = new addPlayer ('timo', 'dhex', 22);
+var newPlayerB = new addPlayer ('musk', 'og', 20);
 
 
 console.log(team._players);
 
+function addGame(opponent, teamPoint, opponentPoints){
+    var game = {}; 
+    game.opponent = opponent;
+	game.teamPoints = teamPoint;
+	game.opponentPoints = opponentPoints;
+    team._games.push(game);
+}
 
 
+var newGame = new addGame('livik',6,1);
+var newGame1 = new addGame('sanhok',3,2);
+var newGame2 = new addGame('erengle',4,1);
+var newGame2 = new addGame('vikendi',5,2);
 
 
+console.log(team._games);
+console.log(team)
 
 
+var totalPoints = 0;
+var totalOpponentPoints = 0;
 
+for(var i=0; i<team._games.length;i++){
+	totalPoints = totalPoints + team._games[i].teamPoints;
+	}
 
+for(var i=0; i<team._games.length;i++){
+	totalOpponentPoints = totalOpponentPoints + team._games[i].opponentPoints;
+	}
 
+function oldestPlayer(){
+	var agePlayer = 0;
+	for(var i=0; i<team._players.length;i++){
+		var newAgePlayer = team._players[i].age;
+			if(newAgePlayer > agePlayer){
+				agePlayer = newAgePlayer;
+		}
+	}
+	console.log(agePlayer);
+}
 
+function alphaPlayer(){
+	var namePlayers = [];
+	for(var i=0; i<team._players.length;i++){
+		var playerName = team._players[i].firstName;
+		namePlayers.push(playerName);
+		}
+	namePlayers.sort();
+	console.log(namePlayers);
+}
 
-
-
-
-
-
-
-// afficher le nombre de voitures de Joe
-
-// changer le nombre de salle de bains de Joe : il n'en possède au'une.
-
-// Joe vient d'acquérir un garage changer la structure pour le refléter.
+console.log('total team points ' + totalPoints);
+console.log("tatal average points " + totalOpponentPoints/team._games.length);
+oldestPlayer();
+alphaPlayer();
